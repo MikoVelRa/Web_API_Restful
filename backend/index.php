8fc97afe31c30e1bbc2b1847f72533dab36afb49
Miguel Arturo Velasco Ramírez <devdayarturo@gmail.com>
2021-04-09 14:29:14 -0500

    second class

--- backend/index.php
+++ backend/index.php
@@ -3,31 +3,37 @@
 
     $datos = file_get_contents('php://input'); //datos por parte del cliente en el flujo de envío
     // print_r($peticion);
+
+    header("Content-Type: application/json; charset=UTF-8");
+
     switch($peticion){
-        case 'GET':
-            if(isset($_GET['id_param'])&&isset($_GET['otro'])){ //soi existen
+        case 'GET': //select
+            if(isset($_GET['id_param']) && isset($_GET['otro'])){ //soi existen
                 print_r($_GET['id_param']);
                 print_r($_GET['otro']);        
             }
             else{
-                print_r($_GET);
+                print_r(file_get_contents("../api/todos.json")); //string
             }
            break;
-        case 'POST':
+
+        case 'POST': //insert
             print_r("El usuario ha enviado una petición POST"); 
             $_POST = json_decode($datos,true);
             /*decodificar los datos del cliente en un 
-            archivo json, para que desde backend se puedan manipular como array asocativo 
+            archivo json, para que desde backend se puedan manipular como array asociativo 
             */
             foreach ($_POST as $llave=>$x) {
                 echo PHP_EOL.$llave." ".$x;
             }
             break;
-        case 'DELETE':
+
+        case 'DELETE': //delete
             print_r("El usuario ha enviado una petición DELETE"); 
             echo "Parámetro: "+$_GET['id_param'];
             break;
-        case 'PUT':
+
+        case 'PUT': //editar
             $_PUT=json_encode($datos,true);
             echo "Parámetro: "+$_GET['id_param'].PHP_EOL;
             
